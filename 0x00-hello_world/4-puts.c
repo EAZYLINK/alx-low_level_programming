6ce62c4a1a83dc6d5e3f1160f3277196a40fe054
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-08 06:59:02 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*The code bellow puts a simple statement on the console*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
