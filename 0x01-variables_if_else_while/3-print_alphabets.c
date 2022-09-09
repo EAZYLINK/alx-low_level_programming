3b4d5a99122a58fb2f9a04a6c0f784e8a1c2bf5f
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-09 08:21:43 -0700

    new commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+char alphabet;
+char ALPHABET;
+alphabet = 'a';
+ALPHABET = 'A';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+}
+while (ALPHABET <= 'Z')
+{
+putchar(ALPHABET);
+}
+putchar('\n');
+}
