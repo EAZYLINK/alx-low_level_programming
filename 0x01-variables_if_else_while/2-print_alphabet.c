6cc7dbaea0d50f67dde84decba92cea56851bf17
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-09 07:28:48 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include (stdlib.h>
+
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+  char alpabet;
+  alphabet = 'a';
+  while(alphabet <= 'z')
+    {
+      putchar("%c ", alphabet);
+      alphabet ++;
+    }
+  return (0);
+}
