b4e11bf45f84463f8508446837c7cddb42c25cfb
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-09 06:56:51 -0700

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %i is %i and is 0\n", n, lastDigit);
 }
 else
 {
-printf("Last digit of %i is %i and is less than 6 and not 0", n, lastDigit);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
