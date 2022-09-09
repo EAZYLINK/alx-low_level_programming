38b62ae7d45ba673759b75ed671ad6e2cb5db3bc
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-09 09:58:48 -0700

    new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+int n = 48;
+char alphabet = 'a';
+while (n < 58)
+{
+putchar(n);
+}
+while (alphabet <= 'f')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
