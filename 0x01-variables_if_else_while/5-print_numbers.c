58b584647285c4d6361b13d8e1e4fa5e3f8c3c7c
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-09 09:05:26 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int num = 0;
+while(num < 10)
+{
+printf(num);
+num++;
+}
+printf("\n");
+return (0);
+}
