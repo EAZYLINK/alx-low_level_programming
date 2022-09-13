41ca29c2ea28df95f14632cf3f2c74a2032a26fd
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 15:04:23 -0700

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check parameter is alphabet
+ * @c: parameter to be checked
+ * Return: either 1 or 0 based on parameter
+ */
+
+int _isalpha(int c)
+{
+if (c >= ('a' || 'A') && c <= ('z' || 'Z');
+return (1);
+else
+return (0);
+}
