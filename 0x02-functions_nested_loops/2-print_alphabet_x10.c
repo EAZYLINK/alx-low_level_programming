7d295353ef513371758ee19c10904d691a2c2a8e
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 13:48:32 -0700

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - prints all the lowercase alphabets 10x
+ * print_alphabet_x10 - prints all the lowercase alphabets 10x
  */
 
 void print_alphabet_x10(void)
 {
 int n = 0;
-char c = 'a';
 while (n < 10)
 {
+char c = 'a';
 while (c <= 'z')
 {
 _putchar(c);
 c++;
 }
-n++;
 _putchar('\n');
+n++;
 }
 }
