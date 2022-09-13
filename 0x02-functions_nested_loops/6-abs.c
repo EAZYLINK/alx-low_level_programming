d90544bfeb6e6492d0089c19916e5cb9daed0ea4
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 15:59:55 -0700

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an int
+ * @int: integer parameter
+ * Return: always positive
+ */
+
+int _abs(int)
+{
+if (int < 0)
+{
+int = int * -1;
+return(int);
+}
+else
+{
+return(int);
+}
+}
