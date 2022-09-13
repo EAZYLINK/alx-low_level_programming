4df4ecd5b3fb3c6fcab2112845914ee4f8b44ed9
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 12:28:02 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c = 'a';
+while(a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+return (0);
+}
