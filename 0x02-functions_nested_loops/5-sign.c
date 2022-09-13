185c1c74e15b30a61a55e3b0f95065e656c7c98d
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 15:43:47 -0700

    new commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a given character
+ * @n: parameter to be tested
+ * Return: either 1, 0 or -1 based on sign of parameter
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+return (1);
+else if (n = 0)
+_putchar('0');
+return (0);
+else
+_putchar('-');
+return (-1);
+}
