a607824b5ced2b982e2bcfe1de37016423eaadf1
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-14 02:28:16 -0700

    new commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,50 +13,17 @@ int largest_number(int a, int b, int c)
 
 int largest;
 
-if (a > b && b > c)
+if (a >= b && b >= c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && a >= c)
 {
 largest = b;
 }
-else if (c > a && a > b)
+else (c >= a && a >= b)
 {
 largest = c;
-}
-else if ( a > b && b < c)
-{
-if (a > c)
-{
-largest = a;
-}
-else
-{
-largest = c;
-}
-}
-else if (b > a && a < c)
-{
-if (b > c)
-{
-largest = b;
-}
-else
-{
-	largest = c;
-}
-}
-else if (c > a && a < b)
-{
-if ( c > b)
-{
-largest = c;
-}
-else
-{
-largest = b;
-}
 }
 
 return (largest);
