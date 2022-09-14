9268e9c1c35dc56017417f7b0afe92282247dbfd
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 17:15:16 -0700

    new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,16 @@
 int print_last_digit(int n)
 {
 int lastDigit;
-lastDigit =  n % 10;
+if (n < 0)
+{
+n = -n;
+lastDigit = n % 10;
+}
+if (lastdigit < 0)
+{
+lastDigit = -lastDigit;
+}
+_putchar(lastDigit + '0');
 return (lastDigit);
 }
+
