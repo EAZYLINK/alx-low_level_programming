94b3d8b39bf430c8fabd752588f6acbaaeb36f87
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-14 01:24:36 -0700

    new commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * negative_or_positive - tests if a parameter is negatice or positive
+ * @1: parameter to be tested
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN.H
+
+#include <stdio.h>
+void negative_or_positive(int i);
+
+#endif /*MAIN.H*/
