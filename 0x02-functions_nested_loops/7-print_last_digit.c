ea9919e0d6e1d8e0a840ff3b473bc9dccee829a3
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-13 17:01:02 -0700

    new commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: given parameter
+ * Return: returns the last digit
+ */
+
+int print_last_digit(int n)
+{
+int n, lastDigit;
+lastDigit =  n % 10;
+return (lastDigit);
+}
