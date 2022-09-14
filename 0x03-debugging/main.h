dc72402794af00a6d6f94e280e8f3047ad115b2f
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-14 15:37:54 +0100

    new commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,10 @@
 #define MAIN.H
 
 #include <stdio.h>
+
 void negative_or_positive(int i);
 int largest_number(int a, int b, int c);
+void print_remianing_days(int, int, int);
+int convert_day(int, int);
 
 #endif /*MAIN.H*/
