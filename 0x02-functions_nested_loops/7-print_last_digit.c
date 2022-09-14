59ff5c0430382c5e2c1f3f46241794778a2822e0
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-14 01:36:22 +0100

    new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,18 @@
 
 int print_last_digit(int n)
 {
-if (n >= 0 && n <= 9)
-{
-return (n);
-}
-else
-{
-n = _abs(n % 10);
-return (n);
-}
+int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
 
