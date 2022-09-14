62053377a475281a4925d20d161730a532810934
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-14 07:33:30 -0700

    new commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,10 @@
 #define MAIN.H
 
 #include <stdio.h>
+
 void negative_or_positive(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /*MAIN.H*/
