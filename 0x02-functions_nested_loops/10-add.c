7872bb23f12fced471feaa7665b495a29e72bbd6
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-14 02:30:53 +0100

    new cpommit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x, @y: two integers to be added
+ * Return: returns the result of the addition
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
