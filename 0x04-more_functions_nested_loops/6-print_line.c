a1a9d4c025ea1b8d60ddf4e144fa95b2039a7173
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 10:33:00 -0700

    new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: parameter
+ * Return: return nothing
+ */
+
+void print_line(int n)
+{
+while ((n != 0) && (n > 0))
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
