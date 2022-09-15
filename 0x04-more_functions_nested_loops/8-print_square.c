4c6685743e9b5a526e0aaecbf468bdb91e805a9f
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 11:37:40 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#including "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: given parameter
+ * Return: return nothing
+ */
+
+voint print_number(int size)
+{
+int n1, n2;
+if (size > 0)
+{
+for (n1 = 0; n1 < size; n1++)
+{
+for (n2 = 0; n2 < (size - 1); n2++)
+{
+_putchar('#');
+}
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
