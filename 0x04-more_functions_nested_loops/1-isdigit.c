8bca89141e6dc0d6e2b661d25cabef7975bc3556
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 02:45:57 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if digit 0 to 9 is entered
+ * @c: parameter to be tested
+ * Return: returns 1 if digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+int c;
+if (c >= 48 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
