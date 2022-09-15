56d4849b94ceb6c3c75188135833ef7c3faa026c
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 08:16:43 -0700

    new commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void negative_or_positive(int i);
 int largest_number(int, int, int);
-void print_remianing_days(int, int, int);
+void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
 #endif /*MAIN_H*/
