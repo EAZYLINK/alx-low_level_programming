fdc751f5f2cc091b7fba08432c93c74b8aa18476
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 09:36:22 -0700

    new commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,13 @@
 
 void print_most_numbers(void)
 {
-int n = 48;
-while ((n < 58) && (n != (2 || 4)))
+for (int n = 48; n < 58; n++)
 {
+if (n == 50 || n == 52)
+{
+continue;
+}
 _putchar(n);
-n++;
 }
 _putchar('\n');
 }
