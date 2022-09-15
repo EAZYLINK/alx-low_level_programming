7d4fdbbfa42a0d0d7ff426407203dfc1a6af4f2a
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 08:51:17 -0700

    new commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print-numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
