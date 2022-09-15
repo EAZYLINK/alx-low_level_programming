77b0b0487bf13f236f96862d720d170afb9995f0
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 13:29:32 -0700

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ for (n = 0; n <= 100; n++)
 {
 if ((n % 3 == 0) && (n != 0))
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else if ((n % 5 == 0) && (n != 0))
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else if ((n % 3 == 0) && (n % 5 == 0) && (n != 0))
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else
 {
