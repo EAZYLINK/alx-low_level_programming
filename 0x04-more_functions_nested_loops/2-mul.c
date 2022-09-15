d294316e3c787c577ffb41ead330e58dc5944980
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-15 08:56:42 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the integers
+ * @a: first integer
+ * @b: second integer
+ * Result: return a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
