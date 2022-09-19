bbe8271ead042f6e24cbb1ed549bfe884bd14d06
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-19 11:03:25 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: parameter
+ */
+
+void puts_half(char *str)
+{
+int counter = 0;
+int i;
+while (counter >= 0)
+{
+if (str[counter] == '\n')
+break;
+counter++;
+}
+if (counter % 2 == 1)
+i = counter / 2;
+else
+i = (counter - 1) / 2;
+for (i++; i < counter; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
