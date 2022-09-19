83e09edf2431ce520766dfd38b38f91bf4b03976
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-19 07:09:54 -0700

    new commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * _strlen: returns the length of a string
- * Return: Always 0
+ * _strlen: function that returns the length of a string
+ * @s: string whose legth if required
+ * Return: returns the length of s
  */
 
-int main(void)
+int _strlen(char *s)
 {
-char *s;
-_strlen(*s);
-return (0);
+int len, count;
+for (len = 0; s[len]; len++)
+{
+count ++;
+}
+return (count);
 }
