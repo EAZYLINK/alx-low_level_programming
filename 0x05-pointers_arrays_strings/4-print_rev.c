a3bc22081c8cadc02724d90bf0c5afbbd2276c52
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-19 09:26:48 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string
+ * @s: paramter
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+int j;
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i-1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
