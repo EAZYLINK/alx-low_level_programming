c0cdbb70af8e7f32be4ef78e4acee932627a81f7
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-19 11:38:49 -0700

    new commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,15 +4,15 @@
  * _strcpy - copies string
  * @dest: first parameter
  * @src: second parameter
- *Return: returns dest
+ * Return: returns dest
  */
 
- char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i;
 for (i = 0; *(src + 1) != '\0'; i++)
 {
-dest[i] = *(src + 1);
+dest[i] = *(src + i);
 }
 dest[i] = '\0';
 return (dest);
