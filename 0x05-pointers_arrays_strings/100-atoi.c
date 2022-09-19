e97343779859d531638715a510a24a1b41f53168
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-19 12:13:41 -0700

    new commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
 int _atoi(char *s)
 {
 unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
-while(*(s + count) != '\0')
+while (*(s + count) != '\0')
 {
 if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
@@ -25,7 +25,7 @@ count++;
 }
 for (i = count - size; i < count; i++)
 {
-j = j + ((*(s + i) - 48) *m);
+j = j + ((*(s + i) - 48) * m);
 m /= 10;
 }
 return (j * k);
