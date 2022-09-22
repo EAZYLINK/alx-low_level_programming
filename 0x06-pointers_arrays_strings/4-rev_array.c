a2fc1ec87e6b70161aa8a36917fb08aa9a70de19
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-22 00:26:56 -0700

    new commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,11 @@
 
 void reverse_array(int *a, int n)
 {
-int *b, i;
+int b, i;
 for (i = 0; i < n; i++)
 {
-b[n-1-i] = a[i];
-}
-for (i = 0; i < n; i++)
-{
-a[i] = b[i];
+b = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = b;
 }
 }
