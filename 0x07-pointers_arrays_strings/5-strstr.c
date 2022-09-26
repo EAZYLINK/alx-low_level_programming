19e7a1af9265522019ba57084cb369b29d9e1b9d
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-26 16:49:06 -0700

    new commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,13 +18,13 @@ i = 0;
 if (haystack[i] == needle[i])
 {
 do {
-if (needle[i - 1] = '\0')
+if (needle[i - 1] == '\0')
 return (haystack);
 i++;
 }
-while (haystack[i] = needle[i])
+while (haystack[i] == needle[i])
 }
 haystack++;
 }
-return('\0');
+return ('\0');
 }
