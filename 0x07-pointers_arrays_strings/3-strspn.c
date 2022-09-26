d11e891ea8ba5c926f67356b7f8578aa445c41e0
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-26 10:35:36 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefixed string
+ * @s: given string
+ * @accept: prefixed string
+ * Return: returns number of bytes
+ */
+
+unsigned int *_strspn(char *s, char *accept)
+{
+unsigned int butes = 0;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+bytes++;
+break;
+}
+else if (accept[i - 1] == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
+}
