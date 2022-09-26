68fda2a9ce61d56e0ce4e8a194543cbf993f6403
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-26 10:20:28 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: given string
+ * @c: required character
+ * Return: returns c if found or NULL if not
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+{}
+if (s[i] == c)
+return (s+i);
+else
+return (NULL);
+}
