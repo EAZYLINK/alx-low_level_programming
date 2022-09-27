4f440710bd6ab1edfc50238d160acc48045e83ab
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-27 04:13:08 +0100

    new commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  * Return: returns pointer or NULL
  */
 
-char *_strstr(char *haystack. char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 int i;
 if (*needle == 0)
@@ -22,7 +22,7 @@ if (needle[i - 1] == '\0')
 return (haystack);
 i++;
 }
-while (haystack[i] == needle[i])
+while (haystack[i] == needle[i]);
 }
 haystack++;
 }
