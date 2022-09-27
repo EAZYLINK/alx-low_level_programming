b0baa7bf59628f8a3a720efe912221ef3fa41d6a
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-27 05:06:31 +0100

    new commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,17 +8,21 @@
 
 void print_diagsums(int *a, int size)
 {
-int i, j;
-int sum = 0;
-for (i = 0; i <= size; i++)
+int i, sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
 {
-for (j = 0; j <= size; j++)
+sum1 += a[i];
+a += size;
+}
+
+a -= size;
+
+for (i = 0; i < size; i++)
 {
-if (i == j || (i + j) == (size + 1))
-sum += a[i][j];
-else
-continue;
+sum2 += a[i];
+a -= size;
 }
-}
-printf("%d", sum);
+
+printf("%d, %d\n", sum1, sum2);
 }
