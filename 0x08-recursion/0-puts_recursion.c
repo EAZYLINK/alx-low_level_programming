71417d300c3ad1330726216db2bcb21b2bd8606a
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-09-28 04:40:12 -0700

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints strings followed by a new line
+ * @s: given string
+ * Return: returns void
+ */
+
+void _puts_recursion(char *s)
+{
+int i = 0;
+if (s[i] == '\0')
+{
+_putchar('\n');
+break;
+}
+_putchar(s[i]);
+i++;
+_puts_recursion(s);
+}
