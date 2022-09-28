f3ea25ae0d3c6d5a750f26ab132145264721aff5
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-28 19:34:10 +0100

    new commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the natural square root
+ * @n: given number
+ * Return: returns the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt(0, n));
+}
+
+int _sqrt(int n, int i)
+{
+if (n > i / 2)
+return (-1);
+else if (n * n == i)
+return (n);
+return (_sqrt(n + 1, i));
+}
