f2060448a3747f1d2a6f2bea70816453aab0a9a7
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-30 21:57:51 +0100

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,9 +3,9 @@
 
 /**
  * main - Entry point
- * @argc: first argument
- * @argv: second argument
- * Return: always 0
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 1 or 0
  */
 
 int main(int argc, char *argv[])
@@ -15,10 +15,17 @@ if (argc == 3)
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-printf("%d\n", a *b);
+printf("%d", a +b);
 return (0);
 }
+else if (argv[1] == '\0')
+{
+printf("%d\n", 0);
+return (0);   
+}
 else
+{
 printf("Error\n");
 return (1);
+}
 }
