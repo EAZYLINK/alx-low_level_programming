f40378c1247d7f47e5c32870f874cafd5b3bd874
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-30 11:23:50 +0100

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+argv[1] = 0;
+printf("%d", argc);
+return (0);
+}
