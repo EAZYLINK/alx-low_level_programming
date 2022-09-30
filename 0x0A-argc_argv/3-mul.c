cc5cb949de561253fbbb3ff508f8bb9405f7e1c5
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-30 15:05:49 +0100

    new commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d", a * b);
+}
+printf("Error\n");
+return (1);
+}
