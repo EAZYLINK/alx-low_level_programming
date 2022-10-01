dc54d547e66b34f836e71f956a8cdae07bcaedf3
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-01 02:57:01 +0100

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+ #include <stdlib.h> 
+ #include <ctype.h> 
+ #include <string.h> 
+  
+ /** 
+  *   * check_num - check - string there are digit 
+  *    * @str: array str 
+  *     * 
+  *      * Return; Always 0 (Success) 
+  *       */ 
+  
+int check_num(char *str)
+{ 
+/*Declaring variables*/ 
+unsigned int count;
+count = 0;
+while (count < strlen(str)) /*count string*/
+{ 
+if (!isdigit(str[count])) /*check if str there are digit*/ 
+{ 
+return (0); 
+}			        
+count++; 
+return (1); 
+} 
 
-/**
- * main - Entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: returns 1 or 0
- */
+ /** 
+  *   * main - Print the name of the program 
+  *    * @argc: Count arguments 
+  *     * @argv: Arguments 
+  *      * 
+  *       * return: Always 0 (Success) 
+  *        */ 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int i, b = 0;
-if (argc >= 3)
+/*Declaring variables*/ 
+int count; 
+int str_to_int; 
+int sum = 0     
+count = 1; 
+while (count < argc) /*Goes through the whole array*/ 
 {
-for (i = 1; i < argc; i++)
-b += atoi(argv[i]);
-printf("%d\n", b);
-return (0);
-}
-else if (atoi(argv[1]) == 0)
-{
-printf("%d\n", 0);
-return (0);   
-}
-else
-{
-printf("Error\n");
-return (1);
-}
+if(check_num(argv[count]))
+{ 
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/ 
+sum += str_to_int; 
+} 
+/*Condition if one of the number contains symbols that are not digits*/ 
+else 
+{ 
+printf("Error\n"); 
+return (1); 
+}   
+count++; 
+printf("%d\n", sum); /*print sum*/ 
+return (0); 
 }
