cc355f67f783c76d59cd68d6d0b200aa073d70b9
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-01 04:20:18 +0100

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,18 @@
 int main(int argc, char *argv[])
 {
 int i, j, sum = 0;
-for (i = 1; i < argc; i++)
+for (i  =  1;  i  <  argc;  i++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
+for (j  =  0; argv[i][j]  !=  '\0'; j++)
 {
-if (argv[i][j] < '0' || argv[i][j] > '9')
+if (argv[i][j]  <  '0'  ||  argv[i][j]  >  '9')
 {
 printf("Error\n");
 return (1);
 }
 }
-sum += atoi(argv[i]);
+sum  +=  atoi(argv[i]);
 }
-printf("%d\n", sum);
+printf("%d\n",  sum);
 return (0);
 }
