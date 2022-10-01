ff08bacd0e404df3ca83970f0849bdc45bd55c0d
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-01 09:12:53 +0100

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h> 
-  
-/** 
-*main - adds positive numbers
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: 0 on success, 1 on failure
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 *main - adds positive numbers
@@ -25,19 +18,11 @@ for (j = 0; argv[i][j] != '\0'; j++)
 if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
-return (1);
+return (1);
 }
 }
-<<<<<<< HEAD
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
 }
-=======
-sum  +=  atoi(argv[i]);
-}
-printf("%d\n",  sum);
-return (0);
-}
->>>>>>> cc355f67f783c76d59cd68d6d0b200aa073d70b9
