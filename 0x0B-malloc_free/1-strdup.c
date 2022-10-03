26f979aee3369e4f48fae6be5bb857b1531d9641
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-04 00:30:51 +0100

    new commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strdup: returns poiter to new array
+ * @str: given string
+ * Return: pointer to new array
+ */
+
+char *_strdup(char *str)
+{
+int count;
+int i;
+char *p;
+if (str == NULL)
+return (NULL);
+for (count = 0; str[count] != '\0'; count++)
+{}
+p = malloc((count +1) * sizeof(char));
+if (p == NULL)
+return (NULL);
+for (i = 0; i <= count + 1; i++)
+{
+p[i] = str[i];
+}
+return (p);
+}
