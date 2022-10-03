b786e199cfab905a58592dd0e15a5ea072e7a504
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-03 23:57:53 +0100

    new commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and initializes it with char c
+ * @size: unsigned integer
+ * @c: character
+ * Return: returns a character
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *p;
+if (size == 0)
+return (NULL);
+p = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+if (p == NULL)
+return (NULL);
+p[i] = c;
+}
+return (p);
+free (p);
+}
