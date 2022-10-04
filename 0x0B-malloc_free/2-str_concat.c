55fb9960313139785072866ccd1998a3cccf05e2
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-04 01:39:13 +0100

    new commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,18 +11,22 @@ char *str_concat(char *s1, char *s2)
 {
 int i, j;
 char *p;
-for (i = 0; s1[i] != '\0' && s1 != NULL; i++)
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i]; i++)
 {}
-for (j = 0; s2[j] != '\0' && s2 != NULL; j++)
+for (j = 0; s2[j]; j++)
 {}
 p = malloc((i + j + 1) * sizeof(char));
 if (p == NULL)
 return (NULL);
-for (i = 0; s1[i] != '\0' && s1 != NULL; i++)
+for (i = 0; s1[i]; i++)
 {
 p[i] = s1[i];
 }
-for (j = 0; s2[j] != '\0' && s2 != NULL; j++)
+for (j = 0; s2[j]; j++)
 {
 p[i + j] = s2[j];
 }
