f8db8a8ead6891d1d771a3d84740ada98cb71158
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-04 08:42:04 -0700

    new commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,25 +10,18 @@
 int **alloc_grid(int width, int height)
 {
 int i, j;
-int **p;
+int *p;
 if (width <= 0 || height <= 0)
 return (NULL);
-p = malloc(height * sizeof(int *));
+p = (int *)malloc((height * width) * sizeof(int));
 if (p == NULL)
 return (NULL);
-for (i = 0; i < height; i++)
+for (i = 0; i < width; i++)
 {
-p[i] = malloc(width * sizeof(int));
-if (p[i] == NULL)
+for (j = 0; j < height; j++)
 {
-for (i--; i >= 0; i--)
-free(p[i]);
-free(p);
-return (NULL);
 }
 }
-for (i = 0; i < height; i++)
-for (j = 0; j < width; j++)
-p[i][j] = 0;
 return (p);
+free(p);
 }
