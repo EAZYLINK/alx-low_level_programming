de1e6e43f86e1185e5221dc5b88f117fa67c7bc7
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-04 01:07:43 +0100

    new commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ int i, j;
 char *p;
 for (i = 0; s1[i] != '\0'; i++)
 {}
-for (j = 0; s2[j] != '\n'; j++)
+for (j = 0; s2[j] != '\0'; j++)
 {}
 p = malloc((i + j + 2) * sizeof(char));
 if (p == NULL)
