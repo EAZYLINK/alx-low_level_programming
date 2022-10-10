f995e5d89988c5b4a9992ba9d4c0e647516f4656
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-10 18:27:35 +0100

    New commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if (d->name == NULL)
-printf("nil");
+printf("nil\n");
 if (d->age < 0)
-printf("nil");
+printf("nil\n");
 if (d->owner == NULL)
-printf("nil");
+printf("nil\n");
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
