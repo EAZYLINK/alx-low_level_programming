a41e02cbe693d0ea35fa5773acb6a3bb5f1ae4ed
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-10 18:31:45 +0100

    Changes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,14 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 printf("nil\n");
+else
+printf("Name: %s\n", d->name);
 if (d->age < 0)
 printf("nil\n");
+else
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("nil\n");
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
+else
 printf("Owner: %s\n", d->owner);
 }
