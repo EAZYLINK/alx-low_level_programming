2acf5410d407360a9826db9e31ba6ff6795e841a
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-13 18:33:46 +0100

    new commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string
+ * @separator: separates string
+ * @n: number of strings to be printed
+ * Return: returns void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list app;
+unsigned int i;
+char *c;
+va_start(app, n);
+for (i = 0; i < n; i++)
+{
+c = va_arg(app, char *);
+if (c == NULL)
+printf("(nil)");
+printf("%s", c);
+if (i != (n-1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(app);
+}
