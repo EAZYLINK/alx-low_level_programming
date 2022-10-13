fd73c48159ee76b77724e60c86c81626d442be23
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-13 04:50:38 +0100

    new commit

--- Ox0F-function_pointers/0-print_name.c
+++ Ox0F-function_pointers/0-print_name.c
@@ -2,13 +2,16 @@
 
 /**
 *print_name - prints a name
-*@name: name to be printed
-*@f: function that prints a name
+*@name:name to print
+*@f: function which print name
 */
 
 void print_name(char *name, void (*f)(char *))
 {
+
 if (name == NULL || f == NULL)
 return;
+
 f(name);
+
 }
