c88fb92c1e9845521b3c7046a7e5148ae8fad8ed
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-20 21:04:35 -0700

    new commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void __attribute__((constructor)) printBefore(void);
+void __attribute__((constructor)) hare(void);
 
 /**
  * printBefore - prints a string before main function is executed
  * Return: void
  */
 
-void printBefore(void)
+void hare(void)
 {
-printf("You're beat! and yet, you must allow, \n"
+printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
 }
