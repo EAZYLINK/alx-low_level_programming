055341e6f0abbbfc741dca381af44ed44c5ac95c
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-20 18:55:43 -0700

    new comtt

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - shows the number of elements in a list
+ * h: list
+ * Return: size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+const list_t *tmp;
+unsigned int count = 0;
+tmp = h;
+while (tmp)
+{
+tmp = tmp->next;
+count++;
+}
+return (count);
+}
