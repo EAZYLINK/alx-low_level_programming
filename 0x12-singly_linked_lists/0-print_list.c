64c8be79afdc03bd8f8a10a9a79d88500978fe9c
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-20 18:29:09 -0700

    new commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,8 @@ size_t print_list(const list_t *h)
 const list_t *tmp;
 unsigned int i;
 tmp = h;
+if (tmp->str == NULL)
+printf("[0] (nil)\n");
 for (i = 0; tmp; i++)
 {
 printf("[%u] %s\n", tmp->len, tmp->str);
