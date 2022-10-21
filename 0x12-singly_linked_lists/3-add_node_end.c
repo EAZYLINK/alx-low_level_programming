2fbe7efa4bca9a736b706613143d4b1ae0426736
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-20 20:12:48 -0700

    new commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+
+/**
+ * add_node_end - adds a node to the end of a list
+ * @head: head node
+ * @str: string to be added
+ * Return: appended list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new, *last;
+int length = 0;
+new = malloc(sizeof(list_t));
+while (str[length])
+length++;
+new->str = strdup(str);
+new->len = length;
+new->next = NULL;
+if (*head == NULL)
+*head = new;
+else
+{
+last = *head;
+while (last->next != NULL)
+last = last->next;
+last->next = new;
+}
+return (*head);
+}
