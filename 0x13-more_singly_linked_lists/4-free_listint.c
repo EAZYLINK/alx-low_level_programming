4018595856d20fce33ede3d2b4ec103b0522b81a
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-10-25 18:58:17 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list memory
+ * @head: pointer to node
+ * Retuurn: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (temp != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
