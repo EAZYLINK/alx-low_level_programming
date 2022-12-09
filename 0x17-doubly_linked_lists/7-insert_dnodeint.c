11bf2185fc79e8fe98821782ad4f440000af3be3
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-12-09 14:07:49 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,36 +9,41 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node;
-unsigned int count;
-if (h == NULL)
-return (NULL);
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-if (idx == 0)
-{
-new_node->next = *h;
-new_node->prev = NULL;
-if (new_node->next != NULL)
-new_node->next->prev = new_node;
-*h = new_node;
-}
-else
-{
-for (count = 0; *h != NULL && count < (idx-1); count++)
-*h = (*h)->next;
-if (*h == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->next = (*h)->next;
-new_node->prev = *h;
-if ((*h)->next != NULL)
-(*h)->next->prev = new_node;
-(*h)->next = new_node;
-}
-return (new_node);
+
+	dlistint_t *newnode;
+	dlistint_t *traverse;
+	unsigned int pos;
+
+	if (h == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = *h;
+		newnode->prev = NULL;
+		if (newnode->next != NULL)
+			newnode->next->prev = newnode;
+		*h = newnode;
+	}
+	else
+	{
+		traverse = *h;
+		for (pos = 0; traverse != NULL && pos < (idx - 1); pos++)
+			traverse = traverse->next;
+		if (traverse == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+		newnode->next = traverse->next;
+		newnode->prev = traverse;
+		if (traverse->next != NULL)
+			traverse->next->prev = newnode;
+		traverse->next = newnode;
+	}
+	return (newnode);
+
 }
