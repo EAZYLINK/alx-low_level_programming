8ab3758b0e47e900eedc6c286b3549c382d4469e
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-12-12 20:25:41 -0800

    new commit

---
+++ 0x18-dynamic_libraries/python_func.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: return a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * add - adds two integers
+ * @x: first parameter
+ * @y: second parameter
+ * Return: returns the result of the addition
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
+
+/**
+ * sub - subtracts y from x
+ * @x: first parameter
+ * @y: second parameter
+ * Return: returns the result of the subtraction
+ */
+
+int sub(int x, int y)
+{
+return (x - y);
+}
+
+/**
+ * div - divides x by y
+ * @x: first parameter
+ * @y: second parameter
+ * Return: returns the result of the division
+ */
+
+int div(int x, int y)
+{
+return (x / y);
+}
+
+/**
+ * mod - finds the modulus of y in x
+ * @x: first parameter
+ * @y: second parameter
+ * Return: returns the result of the modulus
+ */
+
+int mod(int x, int y)
+{
+return (x % y);
+}
+
